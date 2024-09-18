a28f4d6db86034f191b4db4d8ea9e2e5b47d2209
MMdrezaw1383 <mmdrezaw2789@gmail.com>
2024-09-18 22:27:55 +0330

    add templates

--- S4Web/Invoice/Facture.py
+++ S4Web/Invoice/Facture.py
@@ -1,4 +1,14 @@
 from jinja2 import Environment,FileSystemLoader
 import jdatetime
-
+import pdfkit
 env = Environment(loader=FileSystemLoader("templates"))
+template = env.get_template("template.html")
+output = template.render()
+with open(r"templates/newtemplate.html",mode='w',encoding='utf-8') as tm:
+    tm.write(output)
+
+wkhtmltopdf =r""
+file = r"templates/newtemplate.html"
+config = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf)
+pdfkit.from_file(file,output_path="facture.pdf",configuration = config)
+    
