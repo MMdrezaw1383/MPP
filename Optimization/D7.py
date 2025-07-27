dae340783a1e2d5d4142e3790653134ddf6c5f96
Mohammadreza <135561337+MMdrezaw1383@users.noreply.github.com>
2025-07-28 00:25:25 +0330

    Delete Optimization directory

--- Optimization/D7.py
+++
@@ -1,6 +0,0 @@
-def func_(x): x += 2; return x
-
-
-exec("def func(x):\n\tx += 2\n\treturn x ** 2\nprint(func(4))")
-
-exec("def max2(*args):\n\tm=float('-inf')\n\tfor i in args:\n\t\tif i>m:\n\t\t\tm=i\n\treturn m\nprint(max2(1,10,9))")
